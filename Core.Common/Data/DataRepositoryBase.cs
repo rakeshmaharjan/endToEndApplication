02f3b3c6d86f9ac8eb97ee2cf260c0b541731384
unknown <rakeshmaharjan066@gmail.com>
2016-05-06 18:00:30 +0545

    Working on data reposiratory

---
+++ Core.Common/Data/DataRepositoryBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Core.Common.Contracts;
+using System.Data.Entity;
+
+namespace Core.Common.Data
+{
+    public abstract class DataRepositoryBase<T,U> : IDataRepository<T>
+        where T: class, IIdentifiableEntity,new() 
+        where U: DbContext, new()
+    {
+
+
+    }
+}
